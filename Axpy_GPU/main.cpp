4b00e2128aa9cd1658e0e39487c2a79c96d067d4
TvoiSempai <lumpyourhead1@gmail.com>
2018-10-12 23:04:11 +0300

    include

--- Axpy_GPU/main.cpp
+++ Axpy_GPU/main.cpp
@@ -4,6 +4,7 @@
 #include <cstdlib>
 #include <cuda_runtime_api.h>
 #include <iostream>
+#include <omp.h>
 #include "gpu_axpy.cuh"
 // BLAS incy, incx == 1
 
@@ -15,10 +16,28 @@ template<typename T> bool checkResult(T* vec1, T* vec2, int n) {
 	return true;
 }
 
+void saxpy_omp(int n, float a, float *x, int incx, float *y, int incy, float *res) {
+	#pragma omp parallel 
+	{
+	#pragma omp for schedule(guided)
+		for (int i = 0; i < n; ++i) {
+			if ( (i*incx < n) && (i*incy < n) )	res[i*incy] = y[i*incy] + a*x[i*incx];
+		}
+	}
+}
+
+void daxpy_omp(int n, double a, double *x, int incx, double *y, int incy, double *res) {
+	#pragma omp parallel 
+	{
+	#pragma omp for schedule(guided)
+		for (int i = 0; i < n; ++i) {
+			if ( (i*incx < n) && (i*incy < n) )	res[i*incy] = y[i*incy] + a*x[i*incx];
+		}
+	}
+}
 
 
 void saxpy(int n, float a, float *x, int incx, float *y, int incy, float *res) {
-	printf("start saxpy\n");
 	for (int i = 0; i*incy < n; ++i) {
 		if (i*incx < n)	res[i*incy] = y[i*incy] + a*x[i*incx];
 	}
@@ -30,7 +49,7 @@ void daxpy(int n, double a, double *x, int incx, double *y, int incy, double *re
 	}
 }
 
-void showVec(int n, float* vec) {
+template<typename T> void showVec(int n, T* vec) {
 	for (int i = 0; i < n; ++i) {
 		printf("%f\t", vec[i]);
 	}
@@ -40,21 +59,23 @@ void showVec(int n, float* vec) {
 int main(int argc, char **argv) {
 	int N;
 	//float
-	float a, *x, *x_gpu, *y, *y_gpu, *res, *res_gpu, *res_gpu_host, time, time_gpu;
+	float a, *x, *x_gpu, *y, *y_gpu, *res, *res_omp, *res_gpu, *res_gpu_host, time_cpu_f, time_gpu_f, time_omp_f;
+	double a_d, *x_d, *x_gpu_d, *y_d, *y_gpu_d, *res_d, *res_omp_d, res_gpu_d, *res_gpu_host_d, time_cpu_d, time_gpu_d, time_omp_d;
+
 	int incx, incy, blockSize;
 
 	N = atoi(argv[1]); // number of elements in the vector
+	
 	x = (float*)malloc(N * sizeof(float));
 	y = (float*)malloc(N * sizeof(float));
 	res = (float*)malloc(N * sizeof(float));
 	res_gpu_host = (float*)malloc(N * sizeof(float));
+	res_omp = (float*)malloc(N * sizeof(float));
 
 	cudaMalloc((void**)&x_gpu, N * sizeof(float));
 	cudaMalloc((void**)&y_gpu, N * sizeof(float));
 	cudaMalloc((void**)&res_gpu, N * sizeof(float));
 	
-
-
 	incx = atoi(argv[2]);  //increment for x
 	incy = atoi(argv[3]); //increment for y
 	a = atof(argv[4]); // coefficient for sum of vecs
@@ -62,6 +83,17 @@ int main(int argc, char **argv) {
 	//end_of_float
 
 	//double
+	/*a_d = (double)a;
+	x_d = (float*)malloc(N * sizeof(float));
+	y_d = (float*)malloc(N * sizeof(float));
+	res_d = (float*)malloc(N * sizeof(float));
+	res_gpu_host = (float*)malloc(N * sizeof(float));
+
+	cudaMalloc((void**)&x_gpu, N * sizeof(float));
+	cudaMalloc((void**)&y_gpu, N * sizeof(float));
+	cudaMalloc((void**)&res_gpu, N * sizeof(float));
+
+*/
 	/*
 	double a, *x, *y, *res;
 	int incx, incy;
@@ -84,29 +116,33 @@ int main(int argc, char **argv) {
 	for (int i = 0; i < N; ++i) {
 		x[i] = (rand() % 100);
 		y[i] = (rand() % 100);
-		res[i] = (float)0;
-		res_gpu_host[i] = (float)0;
+
+		//res[i] = (float)0;
+		//res_gpu_host[i] = (float)0;
 	}
 	clock_t start_time = clock();
 	saxpy(N, a, x, incx, y, incy, res);
 	clock_t end_time = clock();
-	time = (float)(end_time - start_time) / CLOCKS_PER_SEC;
+	time_cpu_f = (float)(end_time - start_time) / CLOCKS_PER_SEC;
 	
-	
+	start_time = clock();
 	cudaMemcpy(x_gpu, x, N*(sizeof(float)), cudaMemcpyHostToDevice);
 	cudaMemcpy(y_gpu, y, N*(sizeof(float)), cudaMemcpyHostToDevice);
-	//begin of float
-	start_time = clock();
 	saxpy_gpu<<<(blockSize + N) / blockSize, blockSize>>> (N, a, x_gpu, incx, y_gpu, incy, res_gpu);
-	end_time = clock();
 	cudaMemcpy(res_gpu_host, res_gpu, N * sizeof(float), cudaMemcpyDeviceToHost);
-	//end of float
 	cudaDeviceSynchronize();
-	
+	end_time = clock();
+	//end of float
 
 	printf(cudaGetErrorString(cudaGetLastError()));
 	printf("\n");
-	time_gpu = (float)(end_time - start_time) / CLOCKS_PER_SEC;
+	time_gpu_f = (float)(end_time - start_time) / CLOCKS_PER_SEC;
+
+	start_time = clock();
+	saxpy_omp(N, a, x, incx, y, incy, res_omp);
+	end_time = clock();
+	time_omp_f = (float)(end_time - start_time) / CLOCKS_PER_SEC;
+
 	/*printf("x:\n");
 	showVec(N, x);
 	printf("y:\n");
@@ -115,9 +151,11 @@ int main(int argc, char **argv) {
 	showVec(N, res);
 	printf("res_gpu_host:\n");
 	showVec(N, res_gpu_host);*/
-	printf("time: %6.4g\n", time);
-	printf("time_gpu: %10.4g\n", time_gpu);
-	printf(checkResult(res, res_gpu_host, N) ? "Equal": "Not Equal");
+	printf("time_f: %6.4g\n", time_cpu_f);
+	printf("time_gpu_f: %10.4g\n", time_gpu_f);
+	printf("time_omp_f: %10.4g\n", time_omp_f);
+	printf(checkResult(res_omp, res, N) ? "omp_eq\n" : "omp not eq\n");
+	printf(checkResult(res, res_gpu_host, N) ? "Equal\n": "Not Equal\n");
 	cudaFree(x_gpu); cudaFree(y_gpu); cudaFree(res_gpu);
 	free(x); free(y); free(res);
 	return 0;
